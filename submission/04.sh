6b880c8177b317d293fa3ec6536f3124a26a15c3
joie199 <joieama2@gmail.com>
2026-03-20 12:51:09 +0300

    syntax err

--- submission/04.sh
+++ submission/04.sh
@@ -1,4 +1,4 @@
 # Create a wallet with the name "builderswallet".
 bitcoin-cli -regtest createwallet "builderswallet"
 gadd=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
-bitcoin-cli -regtest -rpcwallet=builderswallet generatetoaddress 101 "gadd"
+bitcoin-cli -regtest -rpcwallet=builderswallet generatetoaddress 101 "$gadd"
