8dbdabb5893009c54a4fb545fc0f8f7724312413
joie199 <joieama2@gmail.com>
2026-03-20 12:32:38 +0300

    getbalnace updated

--- submission/05.sh
+++ submission/05.sh
@@ -1,4 +1,2 @@
 # Check the total amount in the wallet.
-gadd=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress) 
-bitcoin-cli -regtest generatetoaddress 101 "$gadd"
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
