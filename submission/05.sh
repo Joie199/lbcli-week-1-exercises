f66f37db299d5eca9440068af07b7a93214f870d
joie199 <joieama2@gmail.com>
2026-03-20 12:12:34 +0300

    getting the balance

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,4 @@
 # Check the total amount in the wallet.
+getadd=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+bitcoin-cli -regtest -rpcwallet=builderswallet generatetoaddress 101 "$getadd"
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
