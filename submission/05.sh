83dfa9d36c64484cb0afe3d9c114e000b4446fc8
joie199 <joieama2@gmail.com>
2026-06-21 20:07:29 +0300

    Update 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -1,4 +1,2 @@
 # Check the total amount in the wallet.
-add=$(bitcoin-cli -regtest getnewaddress)
-bitcoin-cli -regtest generatetoaddress 101 "$add"
-bitcoin-cli -regtest getbalance
+bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
