4c4d01e11efd4372d52ae8619101d3ea041ea84f
joie199 <joieama2@gmail.com>
2026-06-21 22:08:24 +0300

    remove texts

--- submission/05.sh
+++ submission/05.sh
@@ -1,6 +1,3 @@
 # Check the total amount in the wallet.
-set -e
-ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
-bitcoin-cli -regtest -rpcwallet=builderswallet generatetoaddress 101 "$ADDR"
 BALANCE=$(bitcoin-cli -regtest -rpcwallet=builderswallet getbalance)
 echo "$BALANCE"
