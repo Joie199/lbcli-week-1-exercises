68376483dba0ef4ff9d9515f572de86f90c51bea
joie199 <joieama2@gmail.com>
2026-06-21 20:02:31 +0300

    update 2a

--- submission/05.sh
+++ submission/05.sh
@@ -1,4 +1,4 @@
 # Check the total amount in the wallet.
-add= bitcoin-cli -regtest getnewaddress
+add=$(bitcoin-cli -regtest getnewaddress)
 bitcoin-cli -regtest generatetoaddress 101 "$add"
 bitcoin-cli -regtest getbalance
