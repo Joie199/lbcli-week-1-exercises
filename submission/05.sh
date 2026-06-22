4b4a8ee4d59549497c3aa0073101fddd952b0b5e
joie199 <joieama2@gmail.com>
2026-06-23 00:21:44 +0300

    Update 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -1,4 +1,4 @@
 # Check the total amount in the wallet.
-ADDR= bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress ""
+ADDR= bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress
 bitcoin-cli -regtest -rpcwallet=builderswallet generatetoaddress 101 "$ADDR"
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
